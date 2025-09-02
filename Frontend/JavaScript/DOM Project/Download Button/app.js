09652352a03f114fcee44ec4090ad35fb700bd07
Manish Kumar <manishkr.smi@gmail.com>
2025-09-02 23:32:52 +0530

    Create Download Button

---
+++ Frontend/JavaScript/DOM Project/Download Button/app.js
@@ -0,0 +1,22 @@
+var btn = document.querySelector('button');
+
+var load = document.querySelector('#percent');
+var download = document.querySelector('.download');
+var read = document.querySelector('#read');
+
+var grow = 0;
+let clear;
+
+btn.addEventListener('click', function(){
+    setInterval(function(){
+
+        if (grow > 100){
+            clearInterval(clear);
+            btn.innerHTML = 'Downloaded'
+        }else {
+            load.innerHTML = grow++ + "%";
+            read.style.width = grow + "%"
+        }
+        
+    },100)
+})
