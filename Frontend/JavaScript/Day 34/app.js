13dc44536a8209be588495e78c3f87f1cb3c6b25
Manish Kumar <manishkr.smi@gmail.com>
2025-09-17 22:07:59 +0530

    Practice map&filter

--- Frontend/JavaScript/Day 34/app.js
+++ Frontend/JavaScript/Day 34/app.js
@@ -2,11 +2,22 @@
 // github api
 
 
-var btn = document.querySelector('button');
-btn.addEventListener('click', function(){
-        fetch("https://api.github.com/users/async")
-        .then((raw => raw.json())).then(data=>{
-        console.log(data);
-    })
+// var btn = document.querySelector('button');
+// btn.addEventListener('click', function(){
+//         fetch("https://api.github.com/users/async")
+//         .then((raw => raw.json())).then(data=>{
+//         console.log(data);
+//     })
+// })
+
+
+
+let arr = [1,2,3,4,5];
+
+let updatedArr = arr.filter(function(n){
+    if (n > 2){
+        return n;
+    }
 })
+console.log(updatedArr);
 
