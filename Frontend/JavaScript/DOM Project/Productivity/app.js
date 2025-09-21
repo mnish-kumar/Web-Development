f3f27bfc95515251d3bd1d5e2ab7ebdb96b610ab
Manish Kumar <manishkr.smi@gmail.com>
2025-09-21 11:11:17 +0530

    Working On Productivity website

--- Frontend/JavaScript/DOM Project/Productivity/app.js
+++ Frontend/JavaScript/DOM Project/Productivity/app.js
@@ -1,30 +1,88 @@
-function openFeature(){
-    var allElem =  document.querySelectorAll(".elem");
-    var fullElemPage = document.querySelectorAll(".fullElem");
-    var fullElemPageBackButton = document.querySelectorAll(".fullElem .back")
-    
-    allElem.forEach((elem)=> {
-        elem.addEventListener("click", function(){
-            document.querySelectorAll(".fullElem")[elem.id].style.display = "block";
-        })
+function openFeature() {
+  var allElem = document.querySelectorAll(".elem");
+  var fullElemPage = document.querySelectorAll(".fullElem");
+  var fullElemPageBackButton = document.querySelectorAll(".fullElem .back");
+
+  allElem.forEach((elem) => {
+    elem.addEventListener("click", function () {
+      document.querySelectorAll(".fullElem")[elem.id].style.display = "block";
     });
-    
-    
-    fullElemPageBackButton.forEach(function(back){
-        back.addEventListener('click', function(){
-            fullElemPage[back.id].style.display = "none";
-        })
-    })
+  });
+
+  fullElemPageBackButton.forEach(function (back) {
+    back.addEventListener("click", function () {
+      fullElemPage[back.id].style.display = "none";
+    });
+  });
 }
 openFeature();
 
 
-let form = document.querySelector(".addTask form");
 
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    console.log("hello");
-    
-})
+let form = document.querySelector(".addTask form");
+let input = document.querySelector(".addTask form #text-input");
+let detailsInput = document.querySelector(".addTask form textarea");
+let checkboxInput = document.querySelector(".addTask form #check");
+
+let currentTask = [
+//   {
+//     task: "Mandir Jao",
+//     details: "Puja Karne",
+//     imp: true,
+//   },
+];
+
+if(localStorage.getItem("currentTask")){
+  currentTask = JSON.parse(localStorage.getItem("currentTask"));
+}
+
+function renderTask(){
+  var allTask = document.querySelector(".allTask");
+  let sum = "";
+
+    currentTask.forEach((elem, id) => {
+    sum += `
+          <div class="task">
+              <div class="details">
+                <h5>${elem.task} <span class= ${elem.imp}>imp</span></h5>
+                 <p>${elem.details}</p>
+              </div>
+              <button id = ${id}>Mark as complete</button>
+          </div>
+        `;
+    });
+    allTask.innerHTML = sum;
+
+    var markBtn = document.querySelectorAll(".allTask button")
+    markBtn.forEach(function(btn){
+    btn.addEventListener('click', function(){
+        currentTask.splice(btn.id, 1);
+        localStorage.setItem("currentTask", JSON.stringify(currentTask));
+        renderTask();
+    })
+  })
+}
+renderTask();
+
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  currentTask.push({task:input.value, 
+                    details:detailsInput.value, 
+                    imp:checkboxInput.checked
+                });
+
+
+  localStorage.setItem("currentTask", JSON.stringify(currentTask));              
+  input.value = "";  
+  detailsInput.value ="";
+  checkboxInput.checked = false;
+
+  renderTask();
+});
+
+
 
 
