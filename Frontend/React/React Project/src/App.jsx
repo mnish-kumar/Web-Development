8e46a1c9a25ce4991f0fe9a8f17f95ef9678ce21
Manish Kumar <manishkr.smi@gmail.com>
2025-10-15 18:00:41 +0530

    Movie gallery Card

--- Frontend/React/React Project/src/App.jsx
+++ Frontend/React/React Project/src/App.jsx
@@ -1,75 +1,79 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+
+import axios from "axios";
 
 const App = () => {
-  const [title, setTitle] = useState('');
-  const [message, setMessage] = useState('');
-  // title, message ko store karne ke liye
-  const [task, setTask] = useState([]);
+  const [UserData, setUserData] = useState([]);
+  const [index, setindex] = useState(1);
 
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-
-    const copyTask = [...task];
-    copyTask.push({title,message});
-    setTask(copyTask);
-
-    // reset fields after submission
-    setTitle('');
-    setMessage('');
+  const prevHandler = () => {
+    if (index > 1) setindex(index - 1);
+    setUserData([]);
   };
-  const deleteHandler = (idx) =>{
-    const copyTask = [...task];
-    copyTask.splice(idx, 1);
-    setTask(copyTask);
+
+  const nextHandler = () => {
+    setindex(index + 1);
+    setUserData([]);
+  };
+
+  const getData = async () => {
+    const response = await axios.get(
+      `https://picsum.photos/v2/list?page=${index}&limit=10`
+    );
+    setUserData(response.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [index]);
+
+  let printUserData = (
+    <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      Loading...
+    </h3>
+  );
+
+  if (UserData.length > 0) {
+    printUserData = UserData.map((elem) => (
+      <a href={elem.url} target="_blank" key={elem.id}>
+        <div>
+          <div className="w-53 h-55 overflow-hidden rounded-xl">
+            <img
+              className="object-cover w-full h-full"
+              src={elem.download_url}
+              alt={elem.author}
+            />
+          </div>
+          <h3 className="font-bold text-lg text-center">{elem.author}</h3>
+        </div>
+      </a>
+    ));
   }
 
-
   return (
-    <div className='h-screen lg:flex-row flex flex-col gap-6'>
-      <form className=" flex flex-col gap-5 lg:w-1/2 items-start lg:border-r-1 p-5 " onSubmit={submitHandler}>
-        <h1 className='text-4xl text-black font-bold'>Add Task</h1>
+    <div className="min-h-screen flex flex-col justify-between p-5">
+      {/* Image grid container */}
+      <div className="flex flex-wrap gap-10 justify-center relative flex-1">
+        {printUserData}
+      </div>
 
-        <input
-          className="border rounded w-full p-2 outline-0"
-          type="text"
-          placeholder="Enter Task"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-
-        <textarea
-          className="border rounded p-2 w-full h-30 outline-0"
-          type="text"
-          placeholder='Enter details about task'
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-
-        <button className="border rounded px-5 py-2 text-2xl font-medium bg-blue-500 text-white active:scale-95 cursor-pointer w-full">
-          Add Task
+      {/* Buttons always stay bottom */}
+      <div className="flex justify-center items-center gap-5 pt-10">
+        <button
+          onClick={prevHandler}
+          className="bg-amber-400 text-black text-xl px-5 py-2 font-bold active:scale-95 cursor-pointer rounded"
+        >
+          Prev
         </button>
-      </form>
 
-      <div className='lg:w-1/2 pt-3'>
-        <h1 className='text-4xl text-black font-bold'>Recent Task</h1>
-        <div className='flex flex-wrap gap-6 mt-5'>
-          {task.map((data, idx) =>{
-            return <div key={idx} className='h-45 w-45 text-white rounded bg-gray-700 p-2 overflow-hidden flex flex-col items-start justify-between'>
-              <div>
-                <h3 className='text-2xl font-semibold tracking-tighter text-pink-300'>{data.title}</h3>
-                <p className='mt-2 text-xs'>{data.message}</p>
-              </div>
+        <p className="text-xl font-semibold">Page: {index}</p>
 
-              <button
-                onClick={() =>{deleteHandler(idx)}}
-                className='text-xs bg-red-500 py-1 px-2 rounded font-bold active:scale-95'>
-                Delete
-              </button>
-            </div>
-          })}  
-        </div>
-        
+        <button
+          onClick={nextHandler}
+          className="bg-amber-400 text-black text-xl px-5 py-2 font-bold active:scale-95 cursor-pointer rounded"
+        >
+          Next
+        </button>
       </div>
     </div>
   );
